afb67e937a4954b67e35e21400602d42e6ecf8a7
Tkatchenko Margaret <tkatchen@WIN.ecf.utoronto.ca>
2018-09-18 13:30:20 -0400

    Project1 sub2

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,89 +4,55 @@ import java.io.*;
 
 public class FizzBuzz { 
 	
+	public static BufferedReader userin = new BufferedReader (new InputStreamReader (System.in));	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// variables 
-		int f, b, start, end ;
-		BufferedReader userin = new BufferedReader (new InputStreamReader (System.in));
-	//
-
-	//boolean shouldContinue = true;
+	
+		int f, b, start, end; 
 		
 	while (true) { //program restarts unless zero is entered 
-	
 		System.out.println("   JAVA FIZZBUZZ PROGRAM");
 		//system asks for input of f, b, starting number and ending number using buffered reader  	
 		System.out.print("Enter Fizz number: ");
-		f = Integer.parseInt(userin.readLine());
-		if (f == 0) { // break out of while loop
-			//shouldContinue = false; 
-			break;
-		}
-		while (f < 0) { //negative number error 
-			System.out.print("ERROR: Negative numbers are not allowed! Try again:");
-			f = Integer.parseInt(userin.readLine());	
-		}
-		
+		f = getNonNegInt(); //get none negative value of f
+		if (f == 0) break; //break if zero
 		System.out.print("Enter Buzz number: ");
-		b = Integer.parseInt(userin.readLine());
-		if (b == 0) {
-			//shouldContinue = false; 
-			break;
-		}
-		
-		while (b < 0) { //negative number error 
-			System.out.print("ERROR: Negative numbers are not allowed! Try again:");
-			b = Integer.parseInt(userin.readLine());	
-		}
+		b = getNonNegInt(); //get noon negative value of b 
+		if (b == 0) break; //break if zero
 		System.out.print("Enter starting number: ");
-		start =  Integer.parseInt(userin.readLine());
-		
+		start =  Integer.parseInt(userin.readLine()); //get start number 
 		System.out.print("Enter ending number: ");
-		end =  Integer.parseInt(userin.readLine());
-		
-
-		while (end < start) {
+		end =  Integer.parseInt(userin.readLine()); 
+		while (end < start) { //make sure end is greater than start 
 			System.out.print("ERROR: Ending number cannot be less than starting number! Try again:");
 			end =  Integer.parseInt(userin.readLine());
 		}
 		System.out.println();
-	 //variable for for loop 
-		
-	// prints sequential numbers starting at starting number ; check if each is divisible by f or b or f*b
-	//uses f and b from bufferedreader
-	//if divisible by f, print fizz
-	//if divisible by b, print buzz
-	//if divisible by both, print FizzBuzz
-	//end at ending number 
-	//doing this with a loop 
-		
-		
-		while (start <= end) { //number of iterations is 
-			System.out.print(start +". ");
-		if (start % f == 0 && start % b == 0 ) {
-			System.out.println("FizzBuzz");
+	 
+		//counts and put in fizz, buzz or fizzbuzz where appropriate 
+		while (start <= end) { //iterations
+			System.out.print(start +". "); 
+			if (start % f == 0 && start % b == 0 ) System.out.println("FizzBuzz");
+			else if (start % f == 0 ) System.out.println("Fizz");
+			else if (start % b == 0) System.out.println("Buzz");
+			else System.out.println(start);
+			start = start + 1; 
 		}
-		else if (start % f == 0 ) {
-			System.out.println("Fizz");
-		}
-		else if (start % b == 0) {
-			System.out.println("Buzz");
-		}
-		else {
-			System.out.println(start);
-		}
-		start = start + 1; 
-	}
 		System.out.println();
 	}
 	
 	System.out.print("\nThe end.");
 	}
 		
-	//error is negative number entered or if end number is smaller than start number
+//function to get non negative number 
+public static int getNonNegInt() throws NumberFormatException, IOException  {
+	int x;
+	x = Integer.parseInt(userin.readLine());
+	while (x < 0) { //negative number error 
+		System.out.print("ERROR: Negative numbers are not allowed! Try again:");
+		x = Integer.parseInt(userin.readLine());
+	}
 	
-		
+	return x;
 	
-
-	
+}
 }
